766c7eba6030783e1bec573ce6f169da5bfe691b
wani <mill01@naver.com>
2022-04-04 10:59:54 +0900

    Update 2_pandas.py

--- network/2_pandas.py
+++ network/2_pandas.py
@@ -16,3 +16,12 @@ timeserise = str(int(timeserise))
 print(timeserise)
 print(now)
 #################################################  
+
+
+#작업하는 경로(위치)가 어디인지 확인
+print(os.getcwd())
+
+prePath = "./Project/network/"
+file_name = prePath + "input/NewsResult_19900101-20220211.xlsx" 
+
+
