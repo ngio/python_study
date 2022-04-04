496109504df88fda45962d8f46e5a5225bc2de4e
wani <mill01@naver.com>
2022-04-04 11:00:15 +0900

    Update 2_pandas.py

--- network/2_pandas.py
+++ network/2_pandas.py
@@ -25,3 +25,22 @@ prePath = "./Project/network/"
 file_name = prePath + "input/NewsResult_19900101-20220211.xlsx" 
 
 
+# 라이브러리 추가
+import pandas as pd
+import re
+
+# 기사 제목만 별도 텍스트 파일로 저장 
+f = open(prePath + 'input/bigkinds_title01.txt','r', encoding='UTF-8')
+lines = f.readlines()
+f.close()
+
+# 한나눔 불러오기
+from konlpy.tag import Hannanum
+hannanum = Hannanum()
+
+#단어 2차원 리스트
+dataset = []
+for i in range(len(lines)):
+    dataset.append(hannanum.nouns( re.sub('[^가-힣a-zA-Z\s]', '', lines[i] ) ))
+#print(dataset[:10])
+
