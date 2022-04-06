68ceeadb9fa63129d2a3b2cfa10f8f151d7242d3
wani <mill01@naver.com>
2022-04-06 09:05:00 +0900

    Update 1_datacrawring.py

--- DataCrawring/1_datacrawring.py
+++ DataCrawring/1_datacrawring.py
@@ -1,9 +1,21 @@
+"""_summary_
+    json 형태 정보 웹페이지 beautifulsoup4로 가져와서 CSV로 저장하기
+    
+    네이버 시리즈온 영화 전체
+    https://serieson.naver.com/v2/movie/products
+    https://apis.naver.com/seriesOnWeb/serieson-web/v2/movie/products?ero=false&orderType=RECENT_REGISTRATION&offset=0&limit=31&_t=1649039368714
+"""
+import os
+import sys
+os.environ['JAVA_OPTS'] = 'Xmx4096M'
+    
+## 시간 표시  ##################################### 
+import time
+import datetime
+now = datetime.datetime.now()
 
-    #movie_df = pd.DataFrame(dataset, index=view_seq)  # DataFrame에 적재
-    movie_df = pd.DataFrame(dataset)  # DataFrame에 적재
-    #movie_df = movie_df.set_index('title') # 인덱스 설정
-    movie_df = movie_df.set_index('viewSeq') # 인덱스 설정
-    movie_df2 = movie_df.drop_duplicates("synopsis", keep='first')  # 중복제거
-    print(movie_df2.shape)  # .shape를 통해 dataframe의 row와 column 수를 알 수 있다
-    print(len(movie_df2.index)) 
-    print(list(movie_df2.columns))
+timeserise = time.time()
+timeserise = str(int(timeserise))
+print(timeserise)
+print(now)
+#################################################  
