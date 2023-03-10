11b58e7efce48ac942d77e48a5a21b858b6752fe
wani <mill01@naver.com>
2023-03-10 23:46:52 +0900

    Create true_false_quiz.py

---
+++ true_false_quiz.py
@@ -0,0 +1,19 @@
+
+
+"""_summary_
+    What will be the output? 
+    A.True
+    B.False
+    C.Error
+    D.None of above
+"""
+
+a = True
+b = False
+
+print(a or a and b and a)
+
+"""
+    True
+"""
+
