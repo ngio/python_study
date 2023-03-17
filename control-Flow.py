72196b1d7babb4dc0d56de4c494707d966c9eed9
wani <mill01@naver.com>
2023-03-17 10:41:45 +0900

    Update control-Flow.py

--- control-Flow.py
+++ control-Flow.py
@@ -6,3 +6,37 @@
     break
     continue
 """
+
+
+print("----- if, if - else")
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b equal")
+    
+print("----- for")
+fruits = [ "apple", "banana", "cherry"]    
+for x in fruits:
+    print(x)
+    
+
+print("----- while, break")    
+i = 1
+while i < 6:
+    print(i)    
+    if( i == 3 ):
+        break
+    i += 1
+
+print("----- continue")
+i = 0
+while i < 6: 
+    i += 1   
+    if( i == 3 ):
+        continue
+    print(i)
+
+    
+    
