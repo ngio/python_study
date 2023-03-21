21fb2451e3454e0c0f93acd8c4fa058513f44e96
wani <mill01@naver.com>
2023-03-21 12:40:17 +0900

    Update module_py.py

--- module_py.py
+++ module_py.py
@@ -7,6 +7,18 @@ import os
 ret = os.getcwd()
 print(ret, type(ret))
 
+# 파일 이름 변경
+try:
+    os.rename("./before.txt", "./after.txt")
+except:
+    print("파일이 없어요~")
 
 # 파일 이름 변경
-os.rename("C:/Users/hyunh/Desktop/before.txt", "C:/Users/hyunh/Desktop/after.txt")
+os.rename("./after.txt", "./before.txt")
+
+# numpy 모듈의 arange 함수를 사용해서 0.0 부터 5.0까지 0.1씩 증가하는 값을 화면에 출력해보세요.
+import numpy
+for i in numpy.arange(0, 5, 0.1):
+    print(i)
+    
+ 
