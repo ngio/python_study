04482471e7ce89cf1be911c945f7503e727dfa66
wani <mill01@naver.com>
2023-03-28 22:24:59 +0900

    Update 구문_테스트.py

--- 구문_테스트.py
+++ 구문_테스트.py
@@ -5,6 +5,9 @@
     C.47
     D.48
 """
+import secrets
+
+
 def rec(a,b):
     if a == 0:
         return b
@@ -16,9 +19,29 @@ print(rec(8, 12))
 # 48 
 
 
-
-
 s1 = [92,57,[82,75]]
 s2 = s1[:]
 s2[2][1] = 23
 print(s1)
+
+# 클래스 
+result = 0
+
+def add(num):
+    global result
+    result += num
+    return result
+
+print(add(3))
+print(add(4))
+
+
+"""
+    # what will be the output?
+    A.@corporate
+    B.qna
+    C.corporate
+    D.@qna
+"""
+s = ['@corporate','qna']
+print(s[:-1][-1])
