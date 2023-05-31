a097609e28f575149ea695d9fff19525e2fecadc
wani <mill01@naver.com>
2023-05-31 10:31:56 +0900

    Create calendar_info.py
    calendar

---
+++ calendar_info.py
@@ -0,0 +1,11 @@
+# https://docs.python.org/ko/3/library/calendar.html
+import calendar
+
+yy = 2023
+mm = 5
+
+#display the calendar
+print(calendar.month(yy,mm))
+
+#calendar 모듈 내용 확인
+print(dir(calendar))
