07eb8932e13cf817b53d806348b4034fe96fbe42
wani <mill01@naver.com>
2023-08-09 16:50:08 +0900

    Add files via upload

--- GUI_project/ex3-2.py
+++ GUI_project/ex3-2.py
@@ -1,12 +1,19 @@
 ## Ex 3-2. 어플리케이션 아이콘 넣기. QIcon
 ## Ex 3-3. 창 닫기.  QPushButton
 ## Ex 3-4. 툴팁 나타내기.QToolTip
+## Ex 3-5. 상태바 만들기.  QMainWindow
+## Ex 3-6. 메뉴바 만들기.   QAction, qApp
+## Ex 3-7. 툴바 만들기.
 
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtWidgets import QPushButton, QToolTip
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QAction, qApp
+
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import QCoreApplication
+
 
 
 # 2023-05-18 ngio add
@@ -22,34 +29,60 @@ os.chdir(real_path)
 
 
 
-class MyApp(QWidget):
+#class MyApp(QWidget):
+class MyApp(QMainWindow):
 
-  def __init__(self):
-    super().__init__()
-    self.initUI()
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-  def initUI(self):
+    def initUI(self):
     
-    QToolTip.setFont(QFont('SansSerif', 10))
-    self.setToolTip('This is a <b>QWidget</b> widget')
+        #메뉴바
+        exitAction = QAction(QIcon('./img/exit.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+   
+        self.statusBar()
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+        filemenu = menubar.addMenu('&File')
+        filemenu.addAction(exitAction)
     
-    
-    btn = QPushButton('Button', self)
-    btn.setToolTip('This is a <b>QPushButton</b> widget')
-    btn.move(150, 50)
-    btn.resize(btn.sizeHint())
-            
-    btn = QPushButton('Quit', self)
-    btn.setToolTip('This is a <b>Quit Button</b> widget')
-    btn.move(50, 50)
-    btn.resize(btn.sizeHint())
-    btn.clicked.connect(QCoreApplication.instance().quit)
-    
-    #self.setWindowTitle('Icon')
-    self.setWindowIcon(QIcon('./img/web.png'))
-    self.setWindowTitle('Quit Button')
-    self.setGeometry(300, 300, 300, 200)
-    self.show()
+        #툴바
+        exitAction = QAction(QIcon('./img/exit.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+        
+        self.statusBar()
+        self.toolbar = self.addToolBar('Exit')
+        self.toolbar.addAction(exitAction)
+        
+        
+        QToolTip.setFont(QFont('SansSerif', 10))
+        self.setToolTip('This is a <b>QWidget</b> widget')
+        
+        
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.move(150, 150)
+        btn.resize(btn.sizeHint())
+                
+        btn = QPushButton('Quit', self)
+        btn.setToolTip('This is a <b>Quit Button</b> widget')
+        btn.move(50, 150)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(QCoreApplication.instance().quit)
+        
+        self.statusBar().showMessage('Ready')   # 상태바 
+
+        #self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('./img/web.png'))
+        self.setWindowTitle('Quit Button')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
  
   
         
