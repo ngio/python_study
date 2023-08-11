5812cf6a1b2cd91ed99bfe3dacb98205e2b46d2c
wani <mill01@naver.com>
2023-08-11 13:54:43 +0900

    Add files via upload

--- GUI_project/ex3-8.py
+++ GUI_project/ex3-8.py
@@ -3,8 +3,24 @@
 ## Ex 3-9. 날짜와 시간 표시하기.
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QMainWindow 
+from PyQt5.QtWidgets import QApplication,  QDesktopWidget, QMainWindow 
+from PyQt5.QtWidgets import QPushButton, QToolTip
+
+from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtCore import QDate, Qt 
+
+
+# 2023-05-18 ngio add
+# 파이썬 컴파일 경로가 달라서 현재 폴더의 이미지를 호출하지 못할때 작업디렉토리를 변경한다. 
+import os
+#현재 폴더 경로; 작업 폴더 기준
+print(os.getcwd())
+#현재 파일의 폴더 경로; 작업 파일 기준
+real_path = os.path.dirname(os.path.realpath(__file__))
+print(real_path)
+#작업 디렉토리 변경
+os.chdir(real_path)
+
 
 class MyApp(QMainWindow):
 
@@ -15,6 +31,12 @@ class MyApp(QMainWindow):
 
     def initUI(self):        
         self.statusBar().showMessage(self.date.toString(Qt.DefaultLocaleLongDate))
+                    
+        btn = QPushButton('Quit', self)
+        btn.setToolTip('This is a <b>Quit Button</b> widget')
+        btn.move(50, 150)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(QCoreApplication.instance().quit)
         
         self.setWindowTitle('Centering')        
         self.resize(500, 350)
