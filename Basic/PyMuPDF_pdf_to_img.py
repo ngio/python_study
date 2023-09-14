d5446b8d6ffcdbb2fa57930de487f944dd0084fa
wani <mill01@naver.com>
2023-09-14 16:18:11 +0900

    Update PyMuPDF_pdf_to_img.py

--- Basic/PyMuPDF_pdf_to_img.py
+++ Basic/PyMuPDF_pdf_to_img.py
@@ -4,4 +4,17 @@ PyMuPDF로 코딩 없이 PDF에서 이미지 추출
 PyMuPDF
 
 
+pip install PyMuPDF
 
+
+
+import fitz
+doc = fitz.open(PDF_FILE_PATH)
+for i, page in enumerate(doc):
+    img = page.get_pixmap()
+    img.save(f"./data/{i}.png")
+
+
+
+
+python -m fitz extract -images input.pdf
