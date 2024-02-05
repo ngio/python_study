730c9dee3462d9ca506101b0714c33c0277986ae
wani <mill01@naver.com>
2024-02-05 09:16:01 +0900

    Update pytesseract_image_to_text.py

--- pytesseract_image_to_text.py
+++ pytesseract_image_to_text.py
@@ -8,24 +8,48 @@ print(real_path)
 #작업 디렉토리 변경
 os.chdir(real_path) 
 
+"""_summary_
+pip install pillow
+pip install pytesseract
+
+
+
+다운 받아야하는 학습된 한글 데이터 파일명: kor.traineddata
+파일 위치: tesseract가 설치된 경로 C:\Program Files\Tesseract-OCR\tessdata
+
+"""
+
+
 
 from PIL import Image
 import pytesseract  
+import cv2 
+import matplotlib.pyplot as plt
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
+config = ('-l kor+eng --oem 3 --psm 11')
+#config = ('-l kor+eng')
 directory_base = str(real_path)+"./img/"  # 경로object를 문자열로 변경해서 합친다. 
 
         
 # Open an image file
-image_path = directory_base+"02.png"  # Replace with your image file path
+image_path = directory_base+"03_kor_eng.png"  # Replace with your image file path
 img = Image.open(image_path)
 
 # Use Tesseract to extract text
-text = pytesseract.image_to_string(img)
+text = pytesseract.image_to_string(img, config=config)
 
 print("Extracted Text:" + text)
 
+image = cv2.imread(image_path)
+rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+plt.imshow(rgb_image)
+
+# use Tesseract to OCR the image 
+# text = pytesseract.image_to_string(rgb_image, lang='kor+eng')
+text = pytesseract.image_to_string(rgb_image, config=config)
+print(text)
 
 
 if __name__ == "__main__":
@@ -40,7 +64,7 @@ if __name__ == "__main__":
         image_path = directory_base + file  # Replace with your image file path
         img = Image.open(image_path)
 
-        text = pytesseract.image_to_string(img)
+        text = pytesseract.image_to_string(img, config=config)
         print("Extracted Text:")
         print(text)
 
