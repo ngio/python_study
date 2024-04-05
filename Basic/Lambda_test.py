b63fdda7db34d0ae6aad7914e29b10b659b6143b
wani <mill01@naver.com>
2024-04-05 16:03:46 +0900

    Update Lambda_test.py

--- Basic/Lambda_test.py
+++ Basic/Lambda_test.py
@@ -1,6 +1,9 @@
 """  lambda function 
 A lambda function is a small anonymous function.
 A lambda function can take any number of arguments, but can only have one expression.
+
+lambda 매개변수 : 표현식
+함수를 딱 한 줄만으로 만들게 해주는 훌륭한 녀석
 """
 
 x = lambda a : a + 10
