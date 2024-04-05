ea43694b30e2032aacc3786d93a21929def15b15
wani <mill01@naver.com>
2024-04-05 15:58:41 +0900

    Create Lambda_test.py
    Lambda test

---
+++ Basic/Lambda_test.py
@@ -0,0 +1,36 @@
+"""  lambda function 
+A lambda function is a small anonymous function.
+A lambda function can take any number of arguments, but can only have one expression.
+"""
+
+x = lambda a : a + 10
+print(' lambda a : a + 10 => ', x(5))
+
+x = lambda a, b : a * b
+print(' lambda a, b : a * b => ', x(5, 6))
+
+x = lambda a, b, c : a + b + c
+print(' lambda a, b, c : a + b + c => ', x(5, 6, 2))
+ 
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+
+print(mydoubler(11))
+
+def myfunc(n):
+  return lambda a : a * n
+
+mytripler = myfunc(3)
+
+print(mytripler(11))
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
