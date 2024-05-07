9298410b87c3a1578165d9bdf3c4e31f9479be08
wani <mill01@naver.com>
2024-05-07 18:02:32 +0900

    Create str_format.py

---
+++ Basic/Formatting/str_format.py
@@ -0,0 +1,9 @@
+# str.format() 메서드
+# str.format() 메서드를 사용하여 문자열 내에 변수 값을 삽입하는 방식입니다. 포맷 문자열 내에서 중괄호 {}를 사용하여 변수를 지정합니다:
+
+name = "Bob"
+age = 25
+formatted_string = "Name: {}, Age: {}".format(name, age)
+print(formatted_string)
+
+# 위치 인덱스나 변수 이름을 사용하여 더 복잡한 포맷팅을 수행할 수 있습니다.
