bef8233f916d7b42755f66fc9d299b93e4f10d33
wani <mill01@naver.com>
2024-05-07 18:03:46 +0900

    Create format_().py

---
+++ Basic/Formatting/format_().py
@@ -0,0 +1,10 @@
+# format() 함수
+# format() 함수를 사용하여 문자열 포맷을 지정할 수 있습니다. {}를 사용하여 변수를 삽입하고, format() 함수의 인자로 변수 값을 전달합니다:
+
+ 
+name = "David"
+age = 40
+formatted_string = "Name: {}, Age: {}".format(name, age)
+print(formatted_string)
+
+# 포맷 문자열 내에서 변수 순서를 지정할 수 있으며, 포맷 지정자를 사용하여 더 복잡한 포맷팅을 수행할 수 있습니다.
