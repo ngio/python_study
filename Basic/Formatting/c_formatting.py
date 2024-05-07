a1010a8dab8d65b4ad93735cb191ca868c326f03
wani <mill01@naver.com>
2024-05-07 18:01:38 +0900

    Create c_formatting.py

---
+++ Basic/Formatting/c_formatting.py
@@ -0,0 +1,11 @@
+# % 포맷팅 (C 스타일 포맷팅)
+# % 연산자를 사용하여 문자열 내에 변수 값을 삽입하는 방식입니다. 다음은 간단한 예제입니다:
+
+name = "Alice"
+age = 30
+formatted_string = "Name: %s, Age: %d" % (name, age)
+print(formatted_string)
+
+# %s: 문자열 대체
+# %d: 정수 대체
+# %f: 부동 소수점 숫자 대체
