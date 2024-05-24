726ae8ec619f58773f4cd56a31fcbbb4505813ae
wani <mill01@naver.com>
2024-05-24 17:34:14 +0900

    Create 01.py

---
+++ PyramidPattern/01.py
@@ -0,0 +1,16 @@
+"""
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+"""
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows):
+    for j in range(i+1):
+        print("* ", end="")
+    print()
